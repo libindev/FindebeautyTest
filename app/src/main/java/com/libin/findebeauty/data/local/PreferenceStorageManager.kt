baf26d7de4ea300eabfe52b658991b4ecac9e49c
libin <libin.developer@gmail.com>
2025-10-27 20:35:04 +0530

    feat: Add CryptoManager for encryption and decryption
    Adds a `CryptoManager` class to handle symmetric encryption and decryption using the Android Keystore.
    
    This includes:
    - **Keystore Integration**: Uses `AndroidKeyStore` to securely store the encryption key.
    - **Key Management**: Creates and retrieves an `AES` secret key with the alias `auth_token_key`.
    - **Encryption**: Provides an `encrypt` method that uses `AES/CBC/PKCS7` and prepends the Initialization Vector (IV) to the ciphertext.
    - **Decryption**: Provides a `decrypt` method to securely decrypt the data.
    - The class is annotated with `@Inject` for dependency injection.

---
+++ app/src/main/java/com/libin/findebeauty/data/local/PreferenceStorageManager.kt
@@ -0,0 +1,44 @@
+package com.libin.findebeauty.data.local
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.libin.findebeauty.core.CryptoManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.* 
+import javax.inject.Inject
+import kotlin.let
+import kotlin.text.decodeToString
+import kotlin.text.toByteArray
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+class PreferenceStorageManager @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val cryptoManager: CryptoManager
+) {
+
+    companion object {
+        private val TOKEN_KEY = stringPreferencesKey("encrypted_token")
+    }
+
+    fun getToken(): Flow<String?> {
+        return context.dataStore.data.map { preferences ->
+            preferences[TOKEN_KEY]?.let {
+                cryptoManager.decrypt(Base64.getDecoder().decode(it))?.decodeToString()
+            }
+        }
+    }
+
+    suspend fun saveToken(token: String) {
+        val encryptedToken = Base64.getEncoder().encodeToString(cryptoManager.encrypt(token.toByteArray()))
+        context.dataStore.edit {
+            it[TOKEN_KEY] = encryptedToken
+        }
+    }
+}
