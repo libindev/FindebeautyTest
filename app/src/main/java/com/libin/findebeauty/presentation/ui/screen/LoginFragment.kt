1134d04e344515054f283283f2b968b1f88d8955
libin <libin.developer@gmail.com>
2025-10-28 08:30:20 +0530

    refactor: Replace hardcoded token with a constant
    Replaces the hardcoded authentication token in `LoginFragment` with a `DUMMY_TOKEN` constant.
    
    This improves code readability and makes it easier to manage the placeholder token. The token value is moved into a `companion object` and the `saveTokenButton`'s click listener is updated to use this new constant.

--- app/src/main/java/com/libin/findebeauty/presentation/ui/screen/LoginFragment.kt
+++ app/src/main/java/com/libin/findebeauty/presentation/ui/screen/LoginFragment.kt
@@ -23,6 +23,10 @@ class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    private companion object {
+        private const val DUMMY_TOKEN = "" //PASTE YOUR TOKEN HERE
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,10 +39,8 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.saveTokenButton.setOnClickListener {
-            val token =
-                "5zvql-wdk3APxb5uuWOZJcxHDEjnwzc78ex288SFAMm1Pg-CFm9w9kTAx3m6ZCv02lLm7RkgMidXL-mT8q9xsE_wLP1RbDz4hOt8DSMCC187MaJIzH-KO02EFoqhcZ_hcMY273X7HC1CBcFNCVencasSyNT9SY38C3o4vm4XIgN0ceAMQAG9YbnT0xiep_IhKQyXBZeQNlXX7xy8CVQXTPZgxYEMEBhu7QQd-k95yzTBtCsg5Ax7OvZ9kQiKIrJ69Ll7CPuewEm8dMBZknGZeXHCrDoOKzB2Z8vkTF4Fq-PTRiNDo9rnQmeWjTvhBTEnCRAZkF0n6T44L7HyDoyysj4iD5V9lAgSy2PORviKn7W1KxPNlcm3sOK5vFa2UV5QIXmusPtXlMGnczHd8cbPg4skqgcMMk_Sja-as3OMLeZtbxwBVkaTGPh9RckXw-uK"
-            if (token.isNotEmpty()) {
-                viewModel.saveToken(token)
+            if (DUMMY_TOKEN.isNotEmpty()) {
+                viewModel.saveToken(DUMMY_TOKEN)
 
             } else {
                 Toast.makeText(context, "Please enter a token", Toast.LENGTH_SHORT).show()
